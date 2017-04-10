583a4dc0be94b8b105aa243d1a265c4267dde324
Samson Kapeyi <skapeyi@bitbucket.org>
2017-04-10 18:13:21 +0300

    Changes

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -41,7 +41,7 @@ class SmsController extends Controller
 
     public function ait_delivery_callback(Request $request){
       Log::info($request);
-      $message = Sms::where(['message_id' => $request['message_id']]);
+      $message = Sms::where(['message_id' => $request['message_id']])->get();
       $message->status = $request['status'];
       $message->save();
     }
